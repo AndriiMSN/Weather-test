05fe5403e5527ee4a784a2e3cbf7c9cc229b50c6
Andrii Mosin <andrewmosin8@gmail.com>
2020-10-23 13:42:58 +0300

    Updates

---
+++ precache-manifest.086b56fd90e8d1f8abec28decdbfbd83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c532dc576f58aecf3bc2e34a4e7a47d1",
+    "url": "/Weather-test/index.html"
+  },
+  {
+    "revision": "5928a87c6c495c1f001a",
+    "url": "/Weather-test/static/css/main.2a0b700c.chunk.css"
+  },
+  {
+    "revision": "a10a4eed62dc38eda215",
+    "url": "/Weather-test/static/js/2.7dd167ad.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Weather-test/static/js/2.7dd167ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5928a87c6c495c1f001a",
+    "url": "/Weather-test/static/js/main.8990b741.chunk.js"
+  },
+  {
+    "revision": "d2dc8eb891de0de0074f",
+    "url": "/Weather-test/static/js/runtime-main.1b4e1bbc.js"
+  }
+]);
